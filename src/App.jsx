ec238844be9056263356c08b1fe00458fec3bf43
Lucia Morena <luciamorena77@gmail.com>
2022-09-01 17:22:50 -0300

    commit 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,28 @@ import React,{useState,useEffect} from 'react'
 
 function App() {
   const [valor,setValor] = useState(0)
+  const [cor,setCor] = useState('yellow')
+  const [tipo,setTipo] = useState('PROD')
 
   const alterar = () => setValor((Math.random() * 10000000).toFixed())
+
+  const colorir = () => {
+    const cor = '#' + Math.floor(Math.random() * 12345678).toString(16)
+    setCor(cor)
+  }
 
   useEffect(() => alterar(),[])
 
   return (
-    <>
-      <div className="container" onClick={alterar}>
-        <h1>SISTEMA</h1>
+    <div className='container' style={{ background: cor }}>
+      <div className="box" onClick={() => {
+        alterar();
+        colorir();
+      }}>
+        <h1>{tipo}</h1>
         <h2>{valor}</h2>
       </div>
-    </>
+    </div>
   )
 }
 
